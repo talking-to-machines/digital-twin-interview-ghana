d3a61f7466490124548d24cf6e0ec4276203a6f8
raymondlow <iamraymondlow@gmail.com>
2025-05-29 22:06:10 +0100

    Revert user id to test_id to proceed with beta testing and initial release.'

--- app.py
+++ app.py
@@ -876,9 +876,9 @@ def home():
         logging.error(
             "User ID, country, wave or treatment is not provided. Defaulting to 'test_id', 'Ghana', '1' and 'Pilot (NIHR measles)', respectively, for testing purposes."
         )
-        session_user_id = "993373"
-        country = "United Kingdom"
-        wave = "2"
+        session_user_id = "test_id"
+        country = "Ghana"
+        wave = "1"
         treatment = "Pilot (NIHR measles)"
     logging.info(
         f"session_user_id, country, wave, treatment: {session_user_id, country, wave, treatment}"
@@ -997,9 +997,9 @@ def get_bot_response():
         logging.error(
             "User ID, country, wave or treatment is not provided. Defaulting to 'test_id', 'Ghana', '1' and 'Pilot (NIHR measles)', respectively, for testing purposes."
         )
-        session_user_id = "993373"
-        country = "United Kingdom"
-        wave = "2"
+        session_user_id = "test_id"
+        country = "Ghana"
+        wave = "1"
         treatment = "Pilot (NIHR measles)"
     logging.info(
         f"session_user_id, country, wave, treatment: {session_user_id, country, wave, treatment}"
